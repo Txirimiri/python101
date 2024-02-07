a30947ba034be4d4237af97d6f70cb55bf67f12d
Txirimiri <mskas78@gmail.com>
2024-02-07 17:47:40 +0100

    cambios en clase

--- testgit.py
+++ testgit.py
@@ -1,1 +1,13 @@
-print("hola")
+#import sys help("sys") or dir(sys)
+
+
+
+
+""" from math import ceil, floor
+
+x= 5.776
+print(ceil(x))
+print(floor(x)) """
+
+import random
+print(random.randint(4 ,5 ))
