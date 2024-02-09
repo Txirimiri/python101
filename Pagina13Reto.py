c2041b5625a0d9656ea859d004353fd1b577f66a
Kirstie Sundqvist <mskas78@gmail.com>
2024-02-09 17:41:24 +0100

    practice

---
+++ Pagina13Reto.py
@@ -0,0 +1,12 @@
+print("Hola", 2023, "Agur", 5_000_000_000)
+
+x = "Jon"
+y= 32
+print("Hola", x,". Tienes", y ,"años")
+
+hijos = input("¿Cuántos hijos tienes?")
+print(hijos*5)
+
+hijos = int(input("¿Cuántos hijos tienes?"))
+print(hijos*5)
+            
