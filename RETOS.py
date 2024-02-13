daaf9fc95def8b4102d8d37a38640e769dd96522
Kirstie Sundqvist <mskas78@gmail.com>
2024-02-13 14:22:49 +0100

    more code

---
+++ RETOS.py
@@ -0,0 +1,97 @@
+""" print("Programa para practicar input/output")
+
+nombre = input("Introducir tu nombre: ")
+print("Hola" + nombre)
+print(f"Hola {nombre} ")
+
+numero_favorito = int(input("Introducir tu número favorito"))
+print("tu número es " + str(numero_favorito))  """
+
+""" ###Pagina 13 
+nombre = str(input("Hola cual es tu nombre?"))
+edad = int(input("Cuantos años tienes?"))
+print(f"Hola {nombre}, cumplirás {edad + 1}  en tu siguente cumpleaños.")
+print(f"Antes tenías {edad} años.") """
+
+""" 
+ ###Pagina 13
+print("Hola", 2023, "Agur", 5_000_000_000)  """
+
+""" 
+###Pagina 13
+x = input("Hola! Cual es tu nombre?").capitalize()
+y = input("Cuantos años tienes?")
+print("Hola", x, "." ," Tienes", y, "años") """
+
+
+""" ###Pagina 14
+compañeros = input("Con quien trabajas?")
+print(f"Tus compañeros son {compañeros}.")  """
+
+""" ###internet help
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+
+print("Hello", name + ",", "you are", age, "years old and live in", city + ".") """
+
+
+
+""" ###Pagina 13 programa para asegurar es mayor de edad
+
+edad = int(input("Cuantos años tiene?"))
+
+if edad < 18:
+    print("Usted es menor de edad.")
+else:
+     print("Usted es mayor de edad.")
+
+print("Hasta la proxima!")  """ 
+
+
+""" ### Pagina 14
+x, y, z = 5, 10, 15
+print("Coordenadas:", x, y, z)   """
+
+
+""" ###Pagina 14
+compañeros = input("Con quien trabajas?")
+print(f"Tus compañeros son {compañeros}.")  """
+
+
+""" ###internet help
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+
+print("Hello", name + ",", "you are", age, "years old and live in", city + ".") 
+  """
+
+
+""" ###using if/else statements Página 14
+
+a, b, c, d = "Nadal", "Alcaraz", "Badosa", "Muguruza"
+favorite_player = input("Quien es tu jugador favorito? (Nadal, Alcaraz, Badosa, Muguruza): ")
+
+if favorite_player == a: 
+    print("¡Yo también soy fan!")
+elif favorite_player == b:
+    print("Bueno, le falta experiencia todavia.")
+elif favorite_player == c:
+    print("LLeva poco en las pistas después de una lesión.")
+elif favorite_player == d:
+    print("Tiene sus altas y bajas.")
+else:
+    print("No estoy familiarizado con ese jugador de tenis.")
+ """
+
+
+###Pagina 15,16 Tempratura #5 Programa para convertir Celsius a Fahrenheit
+
+temperature = float(input("Enter the temperature you want to convert:"))
+unit_label = input("Is this temperature in Celsius or Fahrenheit? [C/F]")
+    
+    if temperature == "C":
+    print("The temperature in ")
+
+    
