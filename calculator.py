40d9191d9e6e0c54bc8b142409955be4c9aa61df
NZIñaki <in.davila@aulanz.net>
2024-02-20 16:23:28 +0100

    clase

--- calculator.py
+++ calculator.py
@@ -1,4 +1,19 @@
+#caluclator app
+def add(n1, n2):
+    return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 != 0:
+        return n1 / n2
+    else:
+        return "Cannot divide by zero"
+    
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 op = input("Do you want to add, subtract, multiply, or divide? ")
@@ -15,6 +30,7 @@ else:
     result = "Not a valid operation"
 
 print("Result:", result)
+
 
  
 
