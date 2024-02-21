ddb48b6d138735906ae6d9bf32a72c941e0f4836
NZIñaki <in.davila@aulanz.net>
2024-02-21 15:34:06 +0100

    clase

---
+++ longitud.py
@@ -0,0 +1,12 @@
+def calcular_longitud():
+        print("mi function")
+        pass
+if__name==""
+def main():
+        
+
+#def calcular_longitud(nombre:str)->lis[int]:
+ #   """""
+        print("mi funcion" + nombre)
+        return ("hola")#[121.4.5 5.5]
+calcular_longitud()
