b34f21dc7f294a2e8cc488d061ec9f2b47291547
NZIñaki <in.davila@aulanz.net>
2024-02-28 17:51:52 +0100

    clase

--- drinks.py
+++ drinks.py
@@ -1,13 +1,14 @@
 class Bebida:
-    def __init__(self, tipo:str, volumen:int, cantidad:float):
+    def __init__(self, tipo:str, cantidad=100):
             self.tipo = tipo
-            self.volumen = volumen
+            #self.volumen = volumen volumen:int,
             self.cantidad = cantidad
 
     def beber(self, ):
-        print(f"Quiero ordenar {self.tipo} y en {self.cantidad}")
+        print(f"Quiero ordenar {self.tipo} y en {self.cantidad} ml")
 
     
+              
 
 ##### Main program
 
@@ -23,9 +24,14 @@ for i in range(3):
     
     bebida = Bebida(tipo) #---agregar la bebida a la lista 
     listaBebidas.append(bebida)
-    print(listaBebidas)
+
+for bebida in listaBebidas:
+     print(bebida.tipo)
+     print(bebida.cantidad)
 
 
+print(bebida) #__str__ dunder method
+bebida.mostrarBebida()
 #imprimir todas las bebidas
 
     
