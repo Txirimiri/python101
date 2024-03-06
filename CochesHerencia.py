d169b6ab9f44349101dc5ebc9dc9db20d7a921d9
NZIñaki <in.davila@aulanz.net>
2024-03-06 16:51:39 +0100

    clase

--- CochesHerencia.py
+++ CochesHerencia.py
@@ -1,20 +1,27 @@
 class Vehiculo:
-    def __init__(self, marca, modelo, color, ruedas): 
+    def __init__(self, marca, modelo, color): 
         self.marca = marca
         self.modelo = modelo
         self.color = color
-        self.ruedas = ruedas 
+        self.averiado = False
     
     def conducir(self):
          print(f"Soy {self.marca} y vroom, vrooom, vrrooom.")
 
-    def chocar(self):
-         print(f)
+    def chocar(self, vehiculo=False):
+
+        if self.averiado == True:
+        print(f"¡Anda al mecánico!")
+        else:
+        self.averiado == vehiculo
+        print()
+        
+            
 
 
 class Camion(Vehiculo):
-     def __init__(self, marca, modelo, color, ruedas, cabina):
-        super().__init__(marca, modelo, color, ruedas)
+     def __init__(self, marca, modelo, color,  cabina):
+        super().__init__(marca, modelo, color,)
         self.cabina = cabina
 
     def dormir()
