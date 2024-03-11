51ed055cc54ea8d55740393891a63bdc57accf4d
NZIñaki <in.davila@aulanz.net>
2024-03-11 17:14:44 +0100

    clase

--- listcomp.py
+++ listcomp.py
@@ -1,13 +1,7 @@
-frutas = ["MANZANA", "pera", "NARANJA", "uva", "MELON"] #list comprehension
+# frutas = ["MANZANA", "pera", "NARANJA", "uva", "MELON"] #list comprehension
 
-#nuevas_frutas = [fruta.lower() for fruta in frutas if fruta.isupper()] 
-nuevas_frutas = [fruta.lower() for fruta in frutas if fruta.isupper()] ##
-
-
-print(nuevas_frutas)
-numeros 
-
-
+# #nuevas_frutas = [fruta.lower() for fruta in frutas if fruta.isupper()] 
+# nuevas_frutas = [fruta.lower() for fruta in frutas if fruta.isupper()] ##
 
 # nuevas_frutas =[]
 
@@ -16,3 +10,19 @@ numeros
 #         nuevas_frutas.append(fruta)
         
 # print(nuevas_frutas)
+
+
+
+# print(nuevas_frutas)
+
+
+numeros = [3, 54, -12, 4, -67, 99,-120]
+
+#todos =["Par" if i%2==0 else "Impar" for i in numeros]
+#print(todos)
+
+
+listaPositivos = [i for i in numeros if i >=0]
+print (listaPositivos)
+
+#bucle 1st is bucles, assign new list
